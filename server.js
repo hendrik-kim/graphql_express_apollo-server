94bdb443a4dd52361e84a370955fc28514dc1941
Hendrik Kim <48870515+hendrik-kim@users.noreply.github.com>
2021-03-09 20:48:15 -0600

    server & express GraphQL setup

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-v
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema.js');
+
+const app = express();
+
+app.use(
+  '/graphql',
+  expressGraphQL({
+    schema: schema,
+    graphiql: true,
+  })
+);
+
+app.listen(4000, () => {
+  console.log('Server is running');
+});
